e06928cbb3984ae44a2a4130f45c182eee75fdf2
“knakaji” <knakaji@mac.titech.ac.jp>
2026-02-15 18:24:15 +0900

    260215

--- SAWChain/singleChainDynamics_SAW_with_R_Profile_ani_v2.py
+++ SAWChain/singleChainDynamics_SAW_with_R_Profile_ani_v2.py
@@ -3,11 +3,12 @@
 # SAW Chainをモデル化（260128作成開始、260201一旦完了）
 # v2 Reptation開発時に見つけた諸々を実装
 # 末端以外のセグメントを動かす順番をランダムに変更（260208）
+# FuncMを利用する形に変更
 
 import numpy as np
 import matplotlib.pyplot as plt
 import animatplot as amp
-import singleChainDynamicsFunc_SAW_v2 as scd
+import singleChainDynamicsFuncM_SAW_v1 as scdm
 
 try:
     N = int(input('Degree of polymerization (default=100): '))
@@ -20,9 +21,6 @@ except ValueError:
     t_max = 100
 
 t = np.linspace(0, t_max-1, t_max)
-
-x_list_steps = []
-y_list_steps = []
 
 try:
     initConfig = input('Initial Configuration (Fully Extended (F) or Random Coil (R)): ')
@@ -39,37 +37,15 @@ except ValueError:
     centerConfig = "O"
 
 if initConfig == "F": # Fully Extendedからスタートする場合
-    coordinate_list = scd.initConfig_FullExted(N)
-    x_list, y_list = scd.coordinateList2xyList(coordinate_list, N)
-    x_list_steps.append(x_list)
-    y_list_steps.append(y_list)
     plot_lim = 0.6*N
 else: #　Random Coilからスタートする場合
-    coordinate_list = scd.initConfig_Random(N)
-    x_list, y_list = scd.coordinateList2xyList(coordinate_list, N)
-    x_list_steps.append(x_list)
-    y_list_steps.append(y_list)
 #    plot_lim = 3*np.sqrt(N)
     plot_lim = np.sqrt(10*N)
 
-orderedArray = np.arange(1,N)   # 260214追加
+x_list_steps, y_list_steps = scdm.SAWChainMotion(N, t_max, initConfig)
 
-# ステップごとのセグメントの動作
-for rep in range(t_max-1):
-    # まず両末端を動かす
-    coordinate_list = scd.terminalSegment(coordinate_list, N, 0)
-    coordinate_list = scd.terminalSegment(coordinate_list, N, 1)
-    # 次に末端以外のセグメントを動かす
-    shuffledArray = np.random.permutation(orderedArray)   # 260214追加
-    for i in range(N-1):
-#        coordinate_list = scd.segmentMotion(coordinate_list, i+1)                   # こちらが元々
-        coordinate_list = scd.segmentMotion(coordinate_list, shuffledArray[i])      # 260214変更
-    x_list, y_list = scd.coordinateList2xyList(coordinate_list, N)
-    x_list_steps.append(x_list)
-    y_list_steps.append(y_list)
-
-R_list, R_list_steps = scd.end2endDist(x_list_steps, y_list_steps, N, t_max)
-time_steps = scd.timeStep(t)
+R_list, R_list_steps = scdm.end2endDist(x_list_steps, y_list_steps, N, t_max)
+time_steps = scdm.timeStep(t)
 
 # numpyのバージョンアップにより、""ndarray from ragged nested sequences"の制限が厳しくなり、
 # animatplotの途中でエラーが出るようになった。そのための修正が以下の４行
@@ -79,11 +55,11 @@ R_list_steps = np.asanyarray(R_list_steps, dtype=object)
 time_steps = np.asanyarray(time_steps, dtype=object)
 
 # 重心位置
-cx_list, cy_list, cx_list_steps, cy_list_steps = scd.centerOfMass(x_list_steps, y_list_steps, t_max)
+cx_list, cy_list, cx_list_steps, cy_list_steps = scdm.centerOfMass(x_list_steps, y_list_steps, t_max)
 cx_list_steps = np.asanyarray(cx_list_steps, dtype=object)
 cy_list_steps = np.asanyarray(cy_list_steps, dtype=object)
 
-fig_title1 = "Dynamics of a Single Polymer Chain ($N$ = {0})".format(N)
+fig_title1 = "Dynamics of a Single SAW Chain ($N$ = {0})".format(N)
 fig_title2 = "End-to-end Distance, $R$"
 
 fig = plt.figure(figsize=(16,8))
